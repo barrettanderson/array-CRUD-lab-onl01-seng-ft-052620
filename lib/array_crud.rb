2baa380292a6d96854ae5a124c1d03a560c49d86
Barrett Anderson <barrettanderson1989@gmail.com>
2020-05-27 23:59:21 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  [Macaroni]
-  add_element_to_end_of_array << cheese
+  array = [Macaroni]
+  element << cheese
 end
 
 def add_element_to_start_of_array(array, element)
